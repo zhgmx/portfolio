b4ca856f06874537fb13281f3dabd47b70164c6b
polaris <dev@afterlike.org>
2026-08-11 11:30:09 -0400

    perf: omit content bodies from homepage data

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,11 @@
 import { getProjects, getWriting } from '$lib/content';
+import type { Entry } from '$lib/content';
 import type { PageServerLoad } from './$types';
 
+function summary({ body: _body, html: _html, ...entry }: Entry) {
+	return entry;
+}
+
 export const load: PageServerLoad = () => {
-	return { projects: getProjects(), writing: getWriting() };
+	return { projects: getProjects().map(summary), writing: getWriting().map(summary) };
 };
